c23cd3f1a85da681a04f7d1df27095e9688829b8
Kwandes <bz.jasiek@gmail.com>
2021-03-27 13:25:11 +0100

    feat: 🎸 Add a sound on mouse click. I'm sorry

---
+++ src/sounds.js
@@ -0,0 +1,44 @@
+const clickSounds = ["click1.mp3", "click2.mp3"];
+
+const cursedSounds = [
+  "cursed.mp3",
+  "click.mp3",
+  "losing-horn.wav",
+  "mc-damage-taken.wav",
+  "mc-villager-oof.wav",
+  "noice.wav",
+  "oof.wav",
+  "sad-trombone.wav",
+  "satania-scream.wav",
+  "scooby-dooby-doo.mp3",
+  "mc-villager-oof.wav",
+  "wilhelm-scream.wav",
+  "wow.wav",
+];
+
+function playClickSound() {
+  let soundIndex;
+  let audio;
+  // get a random index from the sound list
+  // there are two lists, generic sounds and special sounds. They former has 90% chance to be played, the latter is 10%
+  if (Math.floor(Math.random() * Math.floor(100)) <= 75) {
+    soundIndex = Math.floor(Math.random() * Math.floor(clickSounds.length));
+    audio = new Audio(
+      "./assets/sounds/genericClicks/" + clickSounds[soundIndex]
+    );
+  } else {
+    soundIndex = Math.floor(Math.random() * Math.floor(cursedSounds.length));
+    audio = new Audio("./assets/sounds/easterEggs/" + cursedSounds[soundIndex]);
+  }
+
+  audio.play();
+}
+
+// ensures that the index 0 only has a 20% chance to play even when it gets rolled
+function isGoodIndex(soundIndex) {
+  if (soundIndex == 0) {
+    let temp = Math.floor(Math.random() * Math.floor(100));
+    return temp > 20;
+  }
+  return false;
+}
